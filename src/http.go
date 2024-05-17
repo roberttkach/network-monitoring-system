a139aee2023d2357a7a7f864eb66255cef6ef546
Hallexz <yevtefeevah@gmail.com>
2024-05-17 14:44:04 +0700

    add http

---
+++ src/http.go
@@ -0,0 +1,40 @@
+package src
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+)
+
+var (
+	requests = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests.",
+		},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(requests)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	requests.Inc() // Увеличиваем счетчик запросов.
+	_, err := fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	if err != nil {
+		http.Error(w, "Ошибка при форматировании ответа", http.StatusInternalServerError)
+		return
+	}
+}
+
+func StartServer() {
+	http.Handle("/metrics", promhttp.Handler()) // Экспонируем метрики на /metrics.
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
+	if err != nil {
+		log.Fatalf("Ошибка при запуске сервера: %v", err)
+	}
+}
